8ea97ab0a22e7b1df04747101ec56d10857682bc
unknown <coolsohaib602@gmail.com>
2020-05-17 20:54:52 +0530

    node file update to express

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-let http = require('http');
-let fs = require('fs');
- 
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('file not found');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
- 
-http.createServer(handleRequest).listen(8000);
+'use strict';
+const express = require('express');
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.sendfile('./index.html');
+});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
