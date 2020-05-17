8cb8cfd132abcd8f57aa9a28c59a2675251442ad
unknown <coolsohaib602@gmail.com>
2020-05-17 21:57:23 +0530

    css added inline

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.sendfile('./index.html');
+  res.sendfile('./index.html' );
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
